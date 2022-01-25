292c6756897698ea04054426eec66b670e304c54
linuxCyh <971415706@qq.com>
2022-01-25 19:27:26 +0800

    添加ipv6相关

--- src/Socket/InetAddress.cpp
+++ src/Socket/InetAddress.cpp
@@ -6,23 +6,44 @@
 #include <arpa/inet.h> //inet_pton()
 #include "InetAddress.h"
 
-InetAddress::InetAddress(uint16_t port, bool ifLoopback) {
-    memset(&m_addr, 0, sizeof(m_addr));
-    m_addr.sin_family = AF_INET;
-    in_addr_t ipType = ifLoopback ? INADDR_LOOPBACK : INADDR_ANY;
-    m_addr.sin_addr.s_addr = htobe32(ipType);
-    m_addr.sin_port = htobe16(port);
+InetAddress::InetAddress(uint16_t port, bool ifLoopback, bool ipv6)
+{
+    if (ipv6) {
+        memset(&m_addr6, 0, sizeof(m_addr6));
+        m_addr6.sin6_family = AF_INET6;
+        m_addr6.sin6_addr = ifLoopback ? in6addr_loopback : in6addr_any;
+        m_addr6.sin6_port = htobe16(port);
+    } else {
+        memset(&m_addr, 0, sizeof(m_addr));
+        m_addr.sin_family = AF_INET;
+        in_addr_t ipType = ifLoopback ? INADDR_LOOPBACK : INADDR_ANY;
+        m_addr.sin_addr.s_addr = htobe32(ipType);
+        m_addr.sin_port = htobe16(port);
+    }
 }
 
-InetAddress::InetAddress(const std::string& ip, uint16_t port) {
-    memset(&m_addr, 0, sizeof(m_addr));
-    m_addr.sin_family = AF_INET;
-    inet_pton(AF_INET, ip.c_str(), &m_addr.sin_addr);
-    m_addr.sin_port = htobe16(port);
+InetAddress::InetAddress(const std::string& ip, uint16_t port, bool ipv6)
+{
+    if (ipv6 || strchr(ip.c_str(), ':')) {
+        memset(&m_addr6, 0, sizeof(m_addr6));
+        m_addr6.sin6_family = AF_INET6;
+        inet_pton(AF_INET6, ip.c_str(), &m_addr6.sin6_addr);
+        m_addr6.sin6_port = htobe16(port);
+    } else {
+        memset(&m_addr, 0, sizeof(m_addr));
+        m_addr.sin_family = AF_INET;
+        inet_pton(AF_INET, ip.c_str(), &m_addr.sin_addr);
+        m_addr.sin_port = htobe16(port);
+    }
 }
 
 InetAddress::InetAddress(const sockaddr_in &addr) :
     m_addr(addr)
+{
+}
+
+InetAddress::InetAddress(const sockaddr_in6 &addr) :
+    m_addr6(addr)
 {
 }
 
--- src/Socket/InetAddress.h
+++ src/Socket/InetAddress.h
@@ -11,9 +11,11 @@
 
 class InetAddress {
 public:
-    explicit InetAddress(uint16_t port = 0, bool ifLoopback = false);
-    InetAddress(const std::string& ip, uint16_t port);
+    explicit InetAddress(uint16_t port = 0, bool ifLoopback = false, bool ipv6 = false);
+    InetAddress(const std::string& ip, uint16_t port, bool ipv6 = false);
+
     explicit InetAddress(const struct sockaddr_in& addr);
+    explicit InetAddress(const struct sockaddr_in6& addr);
 
     const struct sockaddr* getSockAddr() const;
     void setSockAddr(struct sockaddr_in& addr);
@@ -23,7 +25,11 @@ public:
     uint16_t getPort() const;
 
 private:
-    sockaddr_in m_addr;
+    union
+    {
+        struct sockaddr_in m_addr;
+        struct sockaddr_in6 m_addr6;
+    };
 };
 
 #endif //NETLIB_INETADDRESS_H
